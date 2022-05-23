ecfdd643db539c3bb041277a8392ef2f2c963cc5
Jingtian Zhang <sam.zhang@desty.app>
2022-05-23 20:24:57 +0800

    fix: drawer component

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -1,1 +1,17 @@
+import "package:flutter/material.dart";
 
+class WingDrawer extends StatefulWidget {
+  const WingDrawer({Key? key}) : super(key: key);
+
+  @override
+  State<WingDrawer> createState() => _WingDrawerState();
+}
+
+class _WingDrawerState extends State<WingDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        // add a listview to the drawer
+        child: ListView(padding: EdgeInsets.zero));
+  }
+}
